bebe18a2147ee0e7f9368f0f24e690ccbccaab09
eZeDaRkeZe <36134529+eZeDaRkeZe@users.noreply.github.com>
2023-03-25 18:48:14 +0300

    Implement FA2

--- fa/program.cs
+++ fa/program.cs
@@ -81,10 +81,65 @@ namespace fans
 
     public class FA2
     {
-        public bool? Run(IEnumerable<char> s)
-        {
-            return false;
-        }
+public static State a = new State()
+    {
+        Name = "a",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State b = new State()
+    {
+        Name = "b",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State c = new State()
+    {
+        Name = "c",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State d = new State()
+    {
+        Name = "d",
+        IsAcceptState = true,
+        Transitions = new Dictionary<char, State>()
+    };
+    public State e = new State()
+    {
+        Name = "e",
+        IsAcceptState = false,
+        Transitions = new Dictionary<char, State>()
+    };
+
+    State InitialState = a;
+
+    public FA2()
+    {
+        a.Transitions['0'] = b;
+        a.Transitions['1'] = e;
+        b.Transitions['0'] = c;
+        b.Transitions['1'] = d;
+        c.Transitions['0'] = b;
+        c.Transitions['1'] = e;
+        d.Transitions['0'] = e;
+        d.Transitions['1'] = b;
+        e.Transitions['0'] = d;
+        e.Transitions['1'] = c;
+    }
+    public bool? Run(IEnumerable<char> s)
+    {
+      return false;
+      State current = InitialState;
+            foreach (var c in s)
+            {
+                current = current.Transitions[c];
+                if (current == null)
+                    return null;
+            }
+            return current.IsAcceptState; 
+    }
+  }
     }
 
     public class FA3
